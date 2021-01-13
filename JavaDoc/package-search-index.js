3345894f6bcfd565c583b17e8db25abc63901d5a
dariusghiura <61147467+dariusghiura@users.noreply.github.com>
2021-01-13 22:04:56 +0200

    Delete JavaDoc directory

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"BusinessLayer"},{"l":"DataLayer"},{"l":"MainClass"},{"l":"PresentationLayer.Admin"},{"l":"PresentationLayer.Chef"},{"l":"PresentationLayer.Waiter"}]
